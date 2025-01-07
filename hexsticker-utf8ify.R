5082f6dba82ec05390f329a000b290ff39f224e8
Roland Krasser <roland.krasser@gmail.com>
2025-01-07 11:22:22 +0100

    update hexsticker

--- hexsticker-utf8ify.R
+++ hexsticker-utf8ify.R
@@ -13,14 +13,14 @@ for (i in seq(1, length(u))) {
 
 # create sticker
 file_out <- "inst/figures/hex_utf8ify.png"
-sticker("inst/figures/utf8ify-background.png",                      # ggplot
+sticker("inst/figures/utf8ify-background2.png",                      # ggplot
         package = "utf8ify",    # name of package
-        p_size = 18,            # size package name
-        p_color = "#F8BC18",    # color package name
-        p_x = 1, p_y = 0.37,     # x/y package name
+        p_size = 38,            # size package name
+        p_color = "white", #F8BC18",    # color package name
+        p_x = 1, p_y = 1,     # x/y package name
         s_x = 1, s_y = 1,    # x/y subplot
         s_width = 1, s_height = 1,
         h_fill = "black", # "#ECEFF1",
         h_color = "darkgrey",
+        spotlight = FALSE,
         filename = file_out)
-
